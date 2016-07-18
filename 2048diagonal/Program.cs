225ed5211f2f0aed0e68c74aa236556faacf32a9
Seung Ho Jang <shjang1992@gmail.com>
2016-07-18 18:53:43 +0900

    Project initial commit

---
+++ 2048diagonal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048diagonal {
+    class Program {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
